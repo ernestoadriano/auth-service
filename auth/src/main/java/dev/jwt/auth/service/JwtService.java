782d7847077ac46d9fa7be2c42c087116fa82785
ernestoadriano <ernestoadriano29@gmail.com>
2026-07-20 15:18:56 +0200

    Delete log

--- auth/src/main/java/dev/jwt/auth/service/JwtService.java
+++ auth/src/main/java/dev/jwt/auth/service/JwtService.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,10 +28,6 @@ public class JwtService {
     @Value("${jwt.refresh-token-expiration-days}")
     private long refreshTokenExpirationDays;
 
-    @PostConstruct
-    public void debugSecret() {
-        System.out.println(">>> JWT SECRET ATUAL: [" + secretKey + "]");
-    }
 
     private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
